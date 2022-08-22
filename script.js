5bfef009446858eba738fe57d79ff8f04368a51d
Costa <eusoude74@gmail.com>
2022-08-22 18:25:08 -0300

    concluido do 1 ao 9

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
 const colorPalette = document.querySelector('#color-palette');
 const pixelBoard = document.querySelector('#pixel-board')
-const btnClearBoard = document.querySelector('#clear-board')
+// const btnClearBoard = document.querySelector('#clear-board')
 const black = 'rgb(0, 0, 0)';
 
 let selectedColor = black;
 
 function getBackgroundColor(event) {
     selectedColor = event.target.style.background
+    const selected = document.querySelector('.selected')
+    selected.classList.remove('selected')
+    event.target.classList.add('selected')
 }
-
-// ARROW FUNCTION
-// const getBackgroundColor = (event) => {
-//     selectedColor = event.target.style.background
-// }
 
 function paint(event) {
     event.target.style.background = selectedColor
 }
-
-// FUNCTION PAINT
-// const paint = (event) => {
-//     event.target.style.background = selectedColor
-// }
 
 function generateRandomColors(index) {
     if (index === 0) {
@@ -33,23 +26,12 @@ function generateRandomColors(index) {
     return `rgb(${r}, ${g}, ${b})`
 }
 
-// ARROW FUNCTION
-// const generateRandomColors = (index) => {
-//     if (index === 0) {
-//         return black
-//     }
-//     const r = Math.floor(Math.random() * 255 + 1)
-//     const g = Math.floor(Math.random() * 255 + 1)
-//     const b = Math.floor(Math.random() * 255 + 1)
-//     return `rgb(${r}, ${g}, ${b})`
-// }
-
 function createPaletteColor() {
     for (let i = 0; i < 4; i++) {
         const div = document.createElement('div');
-        // const btnLimpar = document.createElement('button')
-        // btnLimpar.innerText = 'Limpar';
-        // div.appendChild(btnLimpar)
+        if (i === 0) {
+            div.classList.add('selected')
+        }
         div.addEventListener('click', getBackgroundColor)
         div.classList.add('color');
         div.style.background = generateRandomColors(i)
@@ -64,11 +46,9 @@ function createPixels() {
         div.addEventListener('click', paint);
         div.classList.add('pixel');
         pixelBoard.appendChild(div);
-
     }
 }
 createPixels()
-
 
 const white = 'rgb(255, 255, 255)';
 function pixelsWhite() {
@@ -79,8 +59,6 @@ function pixelsWhite() {
 }
 
 btnClearBoard.addEventListener('click', pixelsWhite)
-
-
 
 
 
