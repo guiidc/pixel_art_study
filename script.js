640065f4c3bb3703b10a4ef0504055c581cec899
costagil <eusoude74@mail.com>
2022-08-24 12:56:01 -0300

    Paleta de Cores 3° Requisito Concluido

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const colorPalette = document.querySelector('#color-palette')
+const black = 'rgb(0, 0, 0)';
 
 function createPaletteColor() {
     for (let i = 0; i < 4; i++){
@@ -10,7 +11,10 @@ function createPaletteColor() {
 }
 createPaletteColor();
 
-function generateRandomColor(){
+function generateRandomColor(index){
+    if (index === 0) {
+        return black
+    }
     const r = Math.floor(Math.random() * 255 + 1)
     const g = Math.floor(Math.random() * 255 + 1)
     const b = Math.floor(Math.random() * 255 + 1)
