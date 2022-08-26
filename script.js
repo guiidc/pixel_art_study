fa15b3c171ed2f6012d3a4f077795d2ce478ab28
costagil <eusoude74@mail.com>
2022-08-26 18:12:27 -0300

    Requisitos do 1 ao 9 concluido

--- script.js
+++ script.js
@@ -1,64 +1,64 @@
 const colorPalette = document.querySelector('#color-palette');
 const pixelBoard = document.querySelector('#pixel-board');
-const btnclearBoard = document.querySelector('#clear-board');
+const btnclearBoard = document.querySelector('#clear-board')
 const black = 'rgb(0, 0, 0)';
 
-function generateRandomColors(index) {
+function selectPalette(event) {
+    const selected = document.querySelector('.selected');
+    selected.classList.remove('selected');
+    event.target.classList.add('selected');
+    selectedColor = event.target.style.background
+}
+
+let selectedColor = black;
+
+function paintPixels(event) {
+    event.target.style.background = selectedColor
+}
+
+function coloringColorPalettes(index) {
     if (index === 0) {
         return black;
     }
     const r = Math.floor(Math.random() * 255 + 1);
     const g = Math.floor(Math.random() * 255 + 1);
     const b = Math.floor(Math.random() * 255 + 1);
-    return `rgb(${r}, ${g}, ${b})`;
+    return `rgb(${r}, ${g}, ${b})`
 }
 
-function getBackgroundColor(event) {
-    selectedColor = event.target.style.background
-    const selected = document.querySelector('.selected');
-    selected.classList.remove('selected');
-    event.target.classList.add('selected');
-}
-
-function createColorPalette() {
+function createcolorPalette() {
     for (let i = 0; i < 4; i++) {
         const div = document.createElement('div');
         if (i === 0) {
             div.classList.add('selected');
         }
-        div.addEventListener('click', getBackgroundColor);
         div.classList.add('color');
         colorPalette.appendChild(div);
-        div.style.background = generateRandomColors(i);
+        div.style.background = coloringColorPalettes(i);
+        div.addEventListener('click', selectPalette);
     }
 }
-createColorPalette();
-
-let selectedColor = black;
-
-function paint(event) {
-    event.target.style.background = selectedColor
-}
+createcolorPalette();
 
 function createPixelsBoard() {
     for (let i = 0; i < 25; i++) {
         const div = document.createElement('div');
         div.classList.add('pixel');
-        div.addEventListener('click', paint);
         pixelBoard.appendChild(div);
+        div.addEventListener('click', paintPixels);
     }
 }
 createPixelsBoard();
 
 const white = 'rgb(255, 255, 255)';
 
-function pixelsWhite(){
-    const allPixels = document.querySelectorAll('.pixel');
-    for (let i = 0; i < allPixels.length; i++){
-        allPixels[i].style.background = white;
-    }
+function colorWhitePaint() {
+    const  allWhiteColor = document.querySelectorAll('.pixel')
+    for (let i = 0; i < allWhiteColor.length; i++)
+    allWhiteColor[i].style.background = white
 }
-btnclearBoard.addEventListener('click', pixelsWhite);
+
+btnclearBoard.addEventListener('click', colorWhitePaint)
 
 
 
